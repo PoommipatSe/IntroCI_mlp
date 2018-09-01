666af38cc570d0b7ba47bddb6aff56a671415e80
UserH61 <poommipat.setthakorn@cmu.ac.th>
2018-09-01 20:50:15 +0700

    epoch training

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import sys
 import pandas as pd
 import numpy as np
 from itertools import repeat
+import math
+
+
 
 input_filename = sys.argv[1]
 DELIMITER = '\t'
 K_F_VALID = 10
 target_num = 8
+a = 0
+n = 0.1
+layer_outline = "8-5-1"
 
 df = pd.read_csv(input_filename, delimiter = DELIMITER, header = 0 )
-""" normalized_df=(df-df.mean())/df.std()
+normalized_df=(df-df.mean())/df.std()
 data_target_mean = df.iloc[:,8].mean()
-data_target_std = df.iloc[:,8].std() """
+data_target_std = df.iloc[:,8].std()
 
+normalized = (df-df.min())/((df.max())-(df.min()))
+df = normalized
 
 def k_fold_cross_valiation(df, K_F_VALID):
     df_fold = df.sample(frac = 1, random_state = 2)
@@ -42,7 +50,6 @@ def generate_train_test_from_dataset(df, K_F_VALID):
     train_set, test_set = k_fold_combine_to_train_test_set(fold_dataset_list, K_F_VALID)
     return train_set, test_set
 
-train_set, test_set = generate_train_test_from_dataset(df,K_F_VALID)
 #print(test_set[0:9])
 
 def seperate_data_to_training_target(df_set, target_num):
@@ -52,13 +59,10 @@ def seperate_data_to_training_target(df_set, target_num):
     df_desire = df_temp.iloc[:,target_num:]
     return df_feed, df_desire
 
-layer_outline = "8-5-1"
 
 def layer_extract_architect(layer_outline):
     layer_outline_list = layer_outline.split("-")
     return layer_outline_list
-
-layer_outline_list = layer_extract_architect(layer_outline)
 
 def weight_init(layer_outline_list):
     np.random.seed(seed=2)
@@ -74,16 +78,16 @@ def weight_init(layer_outline_list):
         w_del_def.append(weight_zero)
     return w_l, w_del_def
 
-def act_fnct_dif(v):
-    return linear_diff_fnct(v)
-
 def act_fnct(v):
-    return linear_fnct(v)
+    return sigmoid(v)
+
+def act_fnct_dif(v):
+    return sigmoid_dif(v)
 
 def linear_fnct(v):
     return v 
 
-def linear_diff_fnct(v):
+def linear_dif_fnct(v):
     try:
         temp = []
         for i in range(0, len(v)):
@@ -92,14 +96,20 @@ def linear_diff_fnct(v):
     except TypeError:
         return 1
 
-w, w_delta_old = weight_init(layer_outline_list)
-working_set = train_set[0]
-working_test_set = test_set[0]
+def sigmoid(x):
+  return 1 / (1 + np.exp(np.multiply(x,-1)))
 
-df_feed, df_desire = seperate_data_to_training_target(working_set,target_num)
+def sigmoid_dif(x):
+  try:
+    return x*(1-x)
+  except TypeError:
+    return np.multiply(x,(np.subtract(1,x))) 
 
-line_in = df_feed.iloc[1].tolist()
-d = df_desire.iloc[1,].tolist()
+def tanh_f(x):
+    return np.tanh(x)
+
+def tanh_f_dif(x):
+    return 1 - np.multiply(np.tanh(x),np.tanh(x))
 
 def feed_forward(line_in):
     step_count = 0
@@ -113,7 +123,7 @@ def feed_forward(line_in):
         for j2 in range(0,int(j)):
             temp = sum(np.multiply(y_out[step_count], w[step_count][j2]))
             v.append(temp)
-        y = act_fnct(v)
+        y = act_fnct(v).tolist()
         y.insert(0,1)
         y_out.append(y)
         step_count += 1
@@ -121,12 +131,12 @@ def feed_forward(line_in):
     y_out[-1].pop(0)
     return y_out
 
-y_out = feed_forward(line_in)
 
 def back_propagation(y_out, d):
     e = np.subtract(d,y_out[-1]) # e = d - y
 
     grdnt_local = np.multiply(e,np.multiply(act_fnct_dif(y_out[-1]),y_out[-1]))
+    grdnt_list = []
     grdnt_list.append(grdnt_local)
 
     step_count = 0
@@ -165,20 +175,39 @@ def back_propagation(y_out, d):
 
         step_count += 1
 
-    return e
+    error_this_pass = sum(np.multiply(e,e))/len(e)
+    return error_this_pass
 
-grdnt_list = []
-
+#initialize
+train_set, test_set = generate_train_test_from_dataset(df,K_F_VALID)
+layer_outline_list = layer_extract_architect(layer_outline)
 layer_outline_list_rv = layer_outline_list[::-1]
 
-a = 0.5
-n = 0.5
+w, w_delta_old = weight_init(layer_outline_list)
 
+working_set = train_set[0]
+working_test_set = test_set[0]
+
+df_feed, df_desire = seperate_data_to_training_target(working_set,target_num)
+
+
+print(df_desire)
+epoch_max = 20
+error_epoch_list_compute_list = []
+for epoch in range(0,epoch_max):
+    error_epoch_list = []
+    for item in range(0,len(df_feed)):
+        line_in = df_feed.iloc[item].tolist()
+        d = df_desire.iloc[item,].tolist()
+        y_out = feed_forward(line_in)
+        e = back_propagation(y_out,d)
+        error_epoch_list.append(e)
+
+    error_epoch_list_compute = sum(error_epoch_list)/len(df_feed)
+    error_epoch_list_compute_list.append(error_epoch_list_compute)
+    print("process : ", epoch ," / ", epoch_max)
+print(error_epoch_list_compute_list)
 print(w)
-e = back_propagation(y_out,d)
-print("xxx")
-print(w)
-print(e)
 
 
 
